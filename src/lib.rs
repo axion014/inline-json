af648d23c55684613ec8df299b3ebbc0f34e6167
axion014 <axion014@gmail.com>
2021-06-24 22:14:32 +0900

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 extern crate proc_macro;
 
-use proc_macro::{TokenStream, TokenTree, Delimiter};
+use proc_macro::{Delimiter, TokenStream, TokenTree};
 use quote::quote;
-use syn::{Type, Expr, parse_macro_input};
+use syn::{parse_macro_input, Expr, Type};
 
 #[proc_macro]
 pub fn json(json: TokenStream) -> TokenStream {
 	let mut iter = json.into_iter().peekable();
-	let ty = iter.by_ref().take_while(|tt| if let TokenTree::Punct(punct) = tt { punct != &',' } else { true }).collect();
+	let ty = iter.by_ref().take_while(is_not_punct(',')).collect();
 	let ty = parse_macro_input!(ty);
 	json_impl(&ty, Box::new(iter))
 }
@@ -20,36 +20,45 @@ fn json_impl<'a>(ty: &Type, iter: Box<dyn Iterator<Item = TokenTree> + 'a>) -> T
 				let mut iter = group.stream().into_iter().peekable();
 				let mut entries = Vec::new();
 				while iter.peek().is_some() {
-					let key = iter.by_ref()
-						.take_while(|tt| if let TokenTree::Punct(punct) = tt { punct != &':' } else { true }).collect();
+					let key = iter.by_ref().take_while(is_not_punct(':')).collect();
 					let key = parse_macro_input!(key as Expr);
-					let value: proc_macro2::TokenStream = json_impl(ty, Box::new(iter.by_ref()
-						.take_while(|tt| if let TokenTree::Punct(punct) = tt { punct != &',' } else { true }))).into();
-					entries.push(quote!{ object.insert((#key).to_string(), #value); });
+					let value: proc_macro2::TokenStream = json_impl(ty, Box::new(iter.by_ref().take_while(is_not_punct(',')))).into();
+					entries.push(quote! { object.insert((#key).to_string(), #value); });
 				}
 				return (quote! {{
 					let mut object = <#ty>::empty_object();
 					#(#entries)*
 					object.into()
-				}}).into();
-			},
+				}})
+				.into();
+			}
 			Delimiter::Bracket => {
 				let mut iter = group.stream().into_iter().peekable();
 				let mut values = Vec::<proc_macro2::TokenStream>::new();
 				while iter.peek().is_some() {
-					values.push(json_impl(ty, Box::new(iter.by_ref()
-						.take_while(|tt| if let TokenTree::Punct(punct) = tt { punct != &',' } else { true }))).into());
+					values.push(json_impl(ty, Box::new(iter.by_ref().take_while(is_not_punct(',')))).into());
 				}
 				return (quote! {{
 					let mut array = <#ty>::empty_array();
 					#(array.push_back(#values);)*
 					array.into()
-				}}).into();
-			},
+				}})
+				.into();
+			}
 			_ => {}
 		}
 	}
 	let expr = iter.collect();
 	let expr = parse_macro_input!(expr as Expr);
 	(quote! { <_ as Into<#ty>>::into(#expr) }).into()
+}
+
+fn is_not_punct(ch: char) -> impl Fn(&TokenTree) -> bool {
+	move |tt| {
+		if let TokenTree::Punct(punct) = tt {
+			punct != &ch
+		} else {
+			true
+		}
+	}
 }
